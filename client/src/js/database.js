ee7697a84f8ca6fd764490058aa4fcce4e085fab
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-02-28 20:33:17 -0600

    install.js copied code from mini-project

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,17 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {};
+export const putDb = async (content) => {
+  console.log('PUT from the JATE database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate','readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({value: content});
+
+  const result = await request;
+  console.log('data saved🚀', result.value);
+  // return result?.value;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
